4626a9cf3880c4046f919ab8e27d5a43762ffdda
Jaden Teng <long.asyn@gmail.com>
2021-09-01 16:57:43 +0800

    Add path Template

---
+++ pathx/template.go
@@ -0,0 +1,92 @@
+package pathx
+
+import (
+	"net/url"
+	"regexp"
+	"strings"
+)
+
+type Template struct {
+	text  string
+	slots [][2]int
+}
+
+var slotRe = regexp.MustCompile(`:[a-zA-Z0-9_-]+`)
+
+// ParseTemplate parses text as a template.
+//
+// Valid text examples:
+//   /:store/products/:product.:ext
+//   /:store.json
+//   /:name
+func ParseTemplate(text string) (*Template, error) {
+	t := &Template{text: text}
+
+	matches := slotRe.FindAllStringSubmatchIndex(text, -1)
+	t.slots = make([][2]int, len(matches))
+	for i, match := range matches {
+		t.slots[i] = [2]int{match[0], match[1]}
+	}
+
+	return t, nil
+}
+
+// MustParseTemplate returns a parsed template or panic.
+func MustParseTemplate(text string) *Template {
+	t, err := ParseTemplate(text)
+	if err != nil {
+		panic(err)
+	}
+	return t
+}
+
+// Execute applies a parsed template to the specified data object.
+//
+// path is same as Path field of url.URL.
+// rawPath is same RawPath field of url.URL.
+func (t *Template) Execute(data map[string]string) (path, rawPath string) {
+	var pathB, rawPathB strings.Builder
+	pathB.Grow(len(t.text))
+	rawPathB.Grow(len(t.text))
+
+	n := 0
+	for _, slot := range t.slots {
+		s, e := slot[0], slot[1]
+
+		lit := t.text[n:s]
+		pathB.WriteString(lit)
+		rawPathB.WriteString(lit)
+
+		val := data[t.text[s+1:e]]
+		pathB.WriteString(val)
+		rawPathB.WriteString(url.PathEscape(val))
+
+		n = e
+	}
+	tail := t.text[n:]
+	pathB.WriteString(tail)
+	rawPathB.WriteString(tail)
+
+	return pathB.String(), rawPathB.String()
+}
+
+// Resolve applies the parsed template to the specified data object
+// and resolves returned path to an absolute URL from an absolute baseURL.
+func (t *Template) Resolve(baseURL *url.URL, data map[string]string) *url.URL {
+	path, rawPath := t.Execute(data)
+	pathURL := &url.URL{Path: path, RawPath: rawPath}
+	if baseURL == nil {
+		return pathURL
+	}
+
+	resolveURL := baseURL.ResolveReference(pathURL)
+	resolveURL.RawQuery = baseURL.RawQuery
+	resolveURL.Fragment = baseURL.Fragment
+	resolveURL.RawFragment = baseURL.RawFragment
+
+	return resolveURL
+}
+
+func (t *Template) String() string {
+	return t.text
+}
---
+++ pathx/template_test.go
@@ -0,0 +1,112 @@
+package pathx
+
+import (
+	"fmt"
+	"net/url"
+	"testing"
+)
+
+func TestTemplate_Execute(t *testing.T) {
+	testCases := []struct {
+		Name string
+
+		Path string
+		Data map[string]string
+
+		ExpectPath    string
+		ExpectRawPath string
+	}{
+		{
+			Name: "empty",
+		},
+
+		{
+			Name: "slash",
+
+			Path: "/",
+
+			ExpectPath:    "/",
+			ExpectRawPath: "/",
+		},
+
+		{
+			Name: "slot",
+
+			Path: "/:store/products/:product.json",
+			Data: map[string]string{
+				"store":   "1001",
+				"product": "2020",
+			},
+
+			ExpectPath:    "/1001/products/2020.json",
+			ExpectRawPath: "/1001/products/2020.json",
+		},
+
+		{
+			Name: "escape",
+
+			Path: "/:name",
+			Data: map[string]string{
+				"name": "中",
+			},
+
+			ExpectPath:    "/中",
+			ExpectRawPath: "/%E4%B8%AD",
+		},
+
+		{
+			Name: "ext",
+
+			Path: "/:product.:ext",
+			Data: map[string]string{
+				"product": "2020",
+				"ext":     "xml",
+			},
+
+			ExpectPath:    "/2020.xml",
+			ExpectRawPath: "/2020.xml",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.Name, func(t *testing.T) {
+			pathTemplate, err := ParseTemplate(tc.Path)
+			if err != nil {
+				t.Fatalf("expect no error, got %v", err)
+			}
+			path, rawPath := pathTemplate.Execute(tc.Data)
+
+			unescapePath, err := url.PathUnescape(rawPath)
+			if err != nil {
+				t.Fatalf("expect no error, got %v", err)
+			}
+			if path != unescapePath {
+				t.Fatalf("expect unescaped path is %q, got %q", path, unescapePath)
+			}
+
+			if tc.ExpectPath != path {
+				t.Fatalf("expect path is %q, got %q", tc.ExpectPath, path)
+			}
+			if tc.ExpectRawPath != rawPath {
+				t.Fatalf("expect raw path is %q, got %q", tc.ExpectRawPath, rawPath)
+			}
+		})
+	}
+}
+
+func ExampleTemplate_Resolve() {
+	baseURL, err := url.Parse("https://www.example.com?foo=bar#/path")
+	if err != nil {
+		fmt.Printf("Parse Base URL failed: %v\n", err)
+		return
+	}
+
+	pathTemplate := MustParseTemplate("/:store/products/:product.json")
+	resolvedURL := pathTemplate.Resolve(baseURL, map[string]string{
+		"store":   "10001",
+		"product": "2020",
+	})
+	fmt.Println("Resolved URL:", resolvedURL)
+	// Output:
+	// Resolved URL: https://www.example.com/10001/products/2020.json?foo=bar#/path
+}
